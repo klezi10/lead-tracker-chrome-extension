d8262688f1df9dda00c0de2aa091488cabf2a93a
klezi10 <74952593+klezi10@users.noreply.github.com>
2021-11-07 13:35:15 +0700

    added eventlistener for results

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+let list = [];
+let inputEl = document.getElementById('input-el');
+let inputBtn = document.querySelector('#save-input-btn');
+let ulEl = document.getElementById('ul-el');
 
+inputBtn.addEventListener('click', function () {
+  ulEl.innerHTML = inputEl.value;
+  inputEl.value = null;
+});
