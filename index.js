3fcb173bc073024647768a3f53928df438b90720
klezi10 <74952593+klezi10@users.noreply.github.com>
2021-11-07 15:34:58 +0700

    set up leads list to loop through and save as list

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-let list = [];
-let inputEl = document.getElementById('input-el');
-let inputBtn = document.querySelector('#save-input-btn');
-let ulEl = document.getElementById('ul-el');
+let leads = [];
+const inputEl = document.getElementById('input-el');
+const inputBtn = document.querySelector('#save-input-btn');
+const ulEl = document.getElementById('ul-el');
 
 inputBtn.addEventListener('click', function () {
-  ulEl.innerHTML = inputEl.value;
+  leads.push(inputEl.value);
   inputEl.value = null;
+  renderLeads();
 });
+
+function renderLeads() {
+  let listItems = '';
+  for (let i = 0; i < leads.length; i++) {
+    listItems += `
+      <li>
+        <a href="${leads[i]}" target="_blank">
+    ${leads[i]}
+       </a>
+     </li>`;
+  }
+  ulEl.innerHTML = listItems;
+}
