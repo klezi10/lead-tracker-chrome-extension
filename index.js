6f1a7f824c8aee79e5496d9dee76e27dcc94c9e6
klezi10 <74952593+klezi10@users.noreply.github.com>
2021-11-14 18:02:46 +0700

    set up API call from chrome for current tab

--- index.js
+++ index.js
@@ -7,18 +7,27 @@ const leadsFromLocalStorage = JSON.parse(localStorage.getItem('savedLeads'));
 const tabBtn = document.getElementById('tab-btn');
 // console.log(leadsFromLocalStorage);
 
-//to show leads after refresh
+//to keep leads after refresh
 if (leadsFromLocalStorage) {
   savedLeads = leadsFromLocalStorage;
   render(savedLeads);
 }
 
-const tabs = [{ url: 'https://www.linkedin.com/in/per-harald-borgen/' }];
-
 tabBtn.addEventListener('click', function () {
-  console.log(tabs[0].url);
+  //api call from chrome
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    savedLeads.push(tabs[0].url);
+
+    //save data in localstorage
+    localStorage.setItem('savedLeads', JSON.stringify(savedLeads));
+    render(savedLeads);
+
+    //to verify that it works
+    // console.log(localStorage.getItem('savedLeads'));
+  });
 });
 
+//to show leads on the page
 function render(leads) {
   let listItems = '';
   for (let i = 0; i < leads.length; i++) {
